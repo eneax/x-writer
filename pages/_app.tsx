dd97e38f3f626938ee69df2ea99d7e7c45641365
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-11-28 21:38:22 +0100

    chore: setup Path Mapping for absolute imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import "styles/globals.css";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
