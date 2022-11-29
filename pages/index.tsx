bba741d10c921f6aca0f8c1760b1ce70af5c8173
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-11-29 09:08:58 +0100

    refactor: Prevent textarea from resizing

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
       <div className="flex flex-col justify-center items-left border-gray-700 py-16">
         <div className="flex flex-col text-left min-h-[calc(100vh-15rem)]">
           <h1 className="mb-4">X-Writer</h1>
-          <p>Write your input below and let us generate the rest.</p>
+          <p>Write your input below and let me generate the rest.</p>
 
           <div className="w-full mb-8">
             <textarea
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
               placeholder="Start typing here..."
               cols={30}
               rows={10}
-              className="block w-full px-4 py-2 bg-gray-800 text-primary-50 border rounded-2xl border-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent"
+              className="block w-full px-4 py-2 bg-gray-800 text-primary-50 border rounded-2xl border-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent resize-none"
               value={userInput}
               onChange={onUserChangedText}
             />
