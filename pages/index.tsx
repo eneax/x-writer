b2505145c87a8e5ae37f53de927c028db494cdef
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-11-29 09:02:18 +0100

    feat: add button below textarea

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,12 @@ const Home: NextPage = () => {
               value={userInput}
               onChange={onUserChangedText}
             />
+
+            <div className="flex flex-col items-start border-gray-700 mt-6">
+              <button className="no-underline font-semibold px-4 py-2.5 rounded-2xl text-black bg-primary-400 hover:bg-primary-400/95 transition duration-300">
+                Generate
+              </button>
+            </div>
           </div>
         </div>
       </div>
