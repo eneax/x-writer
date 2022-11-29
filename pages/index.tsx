0b2163ec1be682b0556318e7893b7a5030abd1a4
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-11-29 08:54:28 +0100

    feat: add React state to textarea field

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,36 @@ import { NextPage } from "next";
 
 import Container from "components/Container";
 
-const Home: NextPage = () => (
-  <Container>
-    <div className="flex flex-col justify-center items-left border-gray-700 py-16">
-      <div className="flex flex-col text-left min-h-[calc(100vh-15rem)]">
-        <h1 className="mb-4">X-Writer</h1>
-        <p>Write your input below and let us generate the rest.</p>
+const Home: NextPage = () => {
+  const [userInput, setUserInput] = React.useState("");
 
-        <div className="w-full mb-8">
-          <textarea
-            name="gpt-3-input"
-            id="gpt-3-input"
-            placeholder="Start typing here..."
-            cols={30}
-            rows={10}
-            className="block w-full px-4 py-2 bg-gray-800 text-primary-50 border rounded-2xl border-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent"
-          />
+  const onUserChangedText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setUserInput(event.target.value);
+  };
+
+  return (
+    <Container>
+      <div className="flex flex-col justify-center items-left border-gray-700 py-16">
+        <div className="flex flex-col text-left min-h-[calc(100vh-15rem)]">
+          <h1 className="mb-4">X-Writer</h1>
+          <p>Write your input below and let us generate the rest.</p>
+
+          <div className="w-full mb-8">
+            <textarea
+              name="gpt-3-input"
+              id="gpt-3-input"
+              placeholder="Start typing here..."
+              cols={30}
+              rows={10}
+              className="block w-full px-4 py-2 bg-gray-800 text-primary-50 border rounded-2xl border-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent"
+              value={userInput}
+              onChange={onUserChangedText}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  </Container>
-);
+    </Container>
+  );
+};
 
 export default Home;
