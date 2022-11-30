cb2ebba7ffd41b8003b78129f657edd7e1113d0e
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-11-30 08:53:17 +0100

    feat: connect frontend to OpenAI and display result

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,19 @@ import Container from "components/Container";
 
 const Home: NextPage = () => {
   const [userInput, setUserInput] = React.useState("");
+  const [apiOutput, setApiOutput] = React.useState("");
+
+  const callGenerateEndpoint = async () => {
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userInput }),
+    });
+    const data = await response.json();
+    const { output } = data;
+
+    setApiOutput(`${output.text}`);
+  };
 
   const onUserChangedText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setUserInput(event.target.value);
@@ -30,11 +43,21 @@ const Home: NextPage = () => {
             />
 
             <div className="flex flex-col items-start border-gray-700 mt-6">
-              <button className="no-underline font-semibold px-4 py-2.5 rounded-2xl text-black bg-primary-400 hover:bg-primary-400/95 transition duration-300">
+              <button
+                className="no-underline font-semibold px-4 py-2.5 rounded-2xl text-black bg-primary-400 hover:bg-primary-400/95 transition duration-300"
+                onClick={callGenerateEndpoint}
+              >
                 Generate
               </button>
             </div>
           </div>
+
+          {apiOutput && (
+            <div className="w-full mb-8">
+              <h2 className="mt-8 -mb-8 underline">Output:</h2>
+              <p className="whitespace-pre-line">{apiOutput}</p>
+            </div>
+          )}
         </div>
       </div>
     </Container>
