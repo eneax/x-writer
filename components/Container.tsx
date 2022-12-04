6e65523e6540b9170f1c059444e9e1128dd14b3d
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-12-04 11:26:31 +0100

    feat: create new navbar with logo

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,5 +1,23 @@
+import * as React from "react";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
+
+const ProfileImage = () => (
+  <div className="rounded-full shadow-lg p-[1px] bg-primary-50 group transform transition ease-out hover:scale-105">
+    <div className="rounded-full p-[1px] h-[36px] w-[36px] transition duration-300 group-hover:scale-105">
+      <Image
+        src="/images/note.png"
+        alt="X-Writer Logo"
+        quality={95}
+        className="rounded-full"
+        width={36}
+        height={36}
+      />
+    </div>
+  </div>
+);
 
 const Container = ({
   children,
@@ -44,6 +62,21 @@ const Container = ({
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
+
+      <nav className="max-w-2xl mx-auto p-8 md:px-0">
+        <div className="flex items-center space-x-6">
+          <Link
+            href="/"
+            title="X-Writer"
+            className="rounded-full flex items-center space-x-2 no-underline"
+          >
+            <ProfileImage />
+            <span className="tracking-tight font-semibold text-xl text-primary-50">
+              X-Writer
+            </span>
+          </Link>
+        </div>
+      </nav>
 
       <main className="max-w-2xl mx-auto flex flex-col justify-center px-8 md:px-0 bg-gray-900">
         {children}
