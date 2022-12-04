a4980a41e96862cd908ab06d0b4e16b709ac7576
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-12-04 11:27:29 +0100

    refactor: add a default user input text to the textarea field

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,10 @@ import { NextPage } from "next";
 import Container from "components/Container";
 import Spinner from "components/Spinner";
 
+const defaultUserInput = "Type here the title of your blog post...";
+
 const Home: NextPage = () => {
-  const [userInput, setUserInput] = React.useState("");
+  const [userInput, setUserInput] = React.useState(defaultUserInput);
   const [apiOutput, setApiOutput] = React.useState("");
   const [isGenerating, setIsGenerating] = React.useState(false);
 
@@ -36,21 +38,14 @@ const Home: NextPage = () => {
     <Container>
       <div className="flex flex-col justify-center items-left border-gray-700 py-16">
         <div className="flex flex-col text-left min-h-screen">
-          <h1 className="mb-4">X-Writer</h1>
-          <p>
-            Write the title of your blog post below and let me generate the
-            rest.
-          </p>
-
           <div className="w-full mb-8">
             <textarea
               name="gpt-3-input"
               id="gpt-3-input"
-              placeholder="Start typing here..."
               cols={30}
               rows={5}
               disabled={isGenerating}
-              className="block w-full px-4 py-2 bg-gray-800 text-primary-50 border rounded-2xl border-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent resize-none"
+              className="block w-full text-lg px-4 py-2 bg-gray-800 text-primary-50 border rounded-2xl border-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent resize-none"
               value={userInput}
               onChange={onUserChangedText}
             />
